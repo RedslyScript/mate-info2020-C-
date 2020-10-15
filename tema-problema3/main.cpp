1480644b070e2075c79f2b82b5bdec1886bf1c90
Redsly <54843353+Redsly@users.noreply.github.com>
2020-10-15 18:10:08 +0300

    Update main.cpp

--- tema-problema3/main.cpp
+++ tema-problema3/main.cpp
@@ -6,12 +6,12 @@ int main()
 {
     float d, v1, v2, suma_km_intr_o_ora, timp_pana_la_intalnire;
     cout << "Se dau doua orase A si B." << endl;
-    cout << "Introdceti distanta (km) dintre cele doua orase. d = ";
+    cout << "Introduceti distanta (km) dintre cele doua orase. d = ";
     cin >> d;
     cout << "Doua automobile pornesc in acelasi timp din cele doua orase." << endl;
-    cout << "Introdceti viteza (km/h) cu care vreti sa se deplaseze primul automobil. v1 = ";
+    cout << "Introduceti viteza (km/h) cu care vreti sa se deplaseze primul automobil. v1 = ";
     cin >> v1;
-    cout << "Introdceti viteza (km/h) cu care vreti sa se deplaseze al doilea automobil. v2 = ";
+    cout << "Introduceti viteza (km/h) cu care vreti sa se deplaseze al doilea automobil. v2 = ";
     cin >> v2;
     suma_km_intr_o_ora = v1 + v2;
     cout << "Cele doua masini, parcurg intr-o ora " << suma_km_intr_o_ora << "km" << endl;
